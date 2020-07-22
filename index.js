e70a8c11588bd7d6b80d965ed7aabaf962e8ba75
Bassel <mail2bassel@yahoo.com>
2020-07-21 21:05:38 -0400

    Refactored files client files into static public folder

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var path = require('path');
+
+const PORT = process.env.PORT || 3050;
 
 var nicknames = {};
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// Static folder route
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log(`socket id ${socket.id} connected`);
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+      console.log(`socket id ${socket.id} disconnected`);
     });
     socket.on('set nickname', nick => {
       nicknames[socket.id] = nick;
+      console.log(`socket id ${socket.id} set nickname to ${nick}`)
     });
     socket.on('chat message', msg => {
       let fullMsg = nicknames[socket.id] + ": " + msg;
       io.emit('chat message', fullMsg);
+      console.log(`socket id ${socket.id} sent msg: ${msg}`);
     });
 });
 
-const PORT = process.env.PORT || 3050;
-
-http.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`);
 });
