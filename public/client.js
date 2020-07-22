33116763220ea9a23ff7c88050ce8c556c88b941
Bassel <mail2bassel@yahoo.com>
2020-07-22 13:01:25 -0400

    Added input focusing, removed button, removed clutter

--- public/client.js
+++ public/client.js
@@ -1,11 +1,26 @@
 var socket = io();
 var nickname;
-console.log("new version ay");
+console.log("22/07/2020");
+
+const myInput = document.getElementById("writeNick");
+$('#writeNick').focus();
+
+myInput.addEventListener("keyup", function(event) {
+  // 13 === 'enter' key
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    setNick();
+  }
+});
+
 function setNick(){
     nickname = document.getElementById('writeNick').value;
     socket.emit('set nickname', nickname);
-    document.getElementById('first').style.display = "none";
-    document.getElementById('second').style.display = "block";
+    $('#first').fadeOut();
+    $('#second').show();
+    $('#m').focus();
+    // document.getElementById('first').style.display = "none";
+    // document.getElementById('second').style.display = "block";
 }
 $(function () {
     $('form').submit(function(e){
